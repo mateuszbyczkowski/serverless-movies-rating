1bf045d10f78f6693d89a9cfe64a69f2fb72b5ac
Mateusz Byczkowski <mateusz.byczkowski@apilia.pl>
2019-06-13 17:54:52 +0200

    Add sample test and codepipeline spec.

---
+++ src/test/java/IntegrationTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IntegrationTests {
+
+    @Test
+    public void handleRequest() {
+        assertEquals(true, true);
+    }
+}
